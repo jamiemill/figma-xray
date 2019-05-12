636bfdb0a6e28db22e640946a5949683ce07d376
Jamie Mill <jamiermill@gmail.com>
2019-05-12 19:53:56 +0100

    Got component report working

--- src/Report.tsx
+++ src/Report.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { FileData } from "./App";
 
+import {componentReportFromComponents} from "./analysis";
+
 function Report({ fileData }: { fileData: FileData }) {
   return fileData ? <div>
     <h1>{fileData.name}</h1>
+    <div>{JSON.stringify(componentReportFromComponents(fileData))}</div>
   </div> : null;
 }
 
