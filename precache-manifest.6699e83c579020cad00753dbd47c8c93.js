e50e1c0679a4c8bb0f3ff2544254660734fe692a
Jamie Mill <jamiermill@gmail.com>
2019-05-19 20:41:06 +0100

    Updates

---
+++ precache-manifest.6699e83c579020cad00753dbd47c8c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7e1c8e0b3a275fa1b8dd758003f9ab",
+    "url": "/figma-xray/index.html"
+  },
+  {
+    "revision": "85e7c3365bd383ddd418",
+    "url": "/figma-xray/static/js/2.351414a1.chunk.js"
+  },
+  {
+    "revision": "76a86d717c07d52439de",
+    "url": "/figma-xray/static/js/main.fb8c9af0.chunk.js"
+  },
+  {
+    "revision": "0a2324deea8275e6d8e7",
+    "url": "/figma-xray/static/js/runtime~main.7c3fcf9c.js"
+  },
+  {
+    "revision": "483685cf173f07a971b16c94360e67fe",
+    "url": "/figma-xray/static/media/ComponentIcon.483685cf.svg"
+  },
+  {
+    "revision": "6e637937e2605440bd0cf98484ee80d5",
+    "url": "/figma-xray/static/media/PageIcon.6e637937.svg"
+  }
+]);
