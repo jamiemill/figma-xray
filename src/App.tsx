27f9fc97538f8aa2ebe1e656c9f4703f706662c9
Jamie Mill <jamiermill@gmail.com>
2019-05-19 14:17:22 +0100

    Factor out File component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import Form, {ApiInfo} from "./Form";
-import Report from "./Report";
+import React, { useState, useEffect } from "react";
+import Form, { ApiInfo } from "./Form";
+import File from "./File";
 import styled from "styled-components";
-import {fetchDocument, FileData, ImageData, fetchImages} from "./api";
-import lint from "./lint";
-import { componentSummary, ComponentSummary } from './analysis';
 
 const Container = styled.div`
   padding: 1em;
@@ -12,67 +9,20 @@ const Container = styled.div`
 
 function App() {
   const [apiInfo, setApiInfo] = useState<ApiInfo>(null);
-  const [fileData, setFileData] = useState<FileData>(null);
-  const [summary, setSummary] = useState<ComponentSummary | null>(null)
-  const [error, setError] = useState<string|null>(null);
-  const [loading, setLoading] = useState<"LOADING_DOCUMENT" | "LOADING_IMAGES" | "LINTING" | "NONE">("NONE");
-  const [imageData, setImageData] = useState<ImageData>(null);
-  const [lintErrors, setLintErrors] = useState<any>(null);
 
-  function handleApiInfoChange(apiInfo:ApiInfo) {
+  function handleApiInfoChange(apiInfo: ApiInfo) {
     setApiInfo(apiInfo);
   }
 
   const fileID: string | null = apiInfo && apiInfo.fileID;
   const personalToken: string | null = apiInfo && apiInfo.personalToken;
 
-  useEffect(() => {
-    if(fileID && personalToken) {
-      const loadEverything = async () => {
-        setLoading("LOADING_DOCUMENT");
-        setFileData(null);
-        setImageData(null);
-        setError(null);
-        const fileData = await fetchDocument(fileID, personalToken);
-        setFileData(fileData);
-
-        if (fileData) {
-          const summary = componentSummary(fileData);
-          setSummary(summary);
-        }
-        
-        // setLoading("LINTING");
-        // const lintErrors = await lint(fileData);
-        // setLintErrors(lintErrors);
-
-        setLoading("LOADING_IMAGES");
-        const componentIds = fileData ? Object.keys(fileData.components) : [];
-        const images = await fetchImages(fileID, personalToken, componentIds);
-        setImageData(images);
-
-        setLoading("NONE");
-      }
-      loadEverything().catch((e) => {
-        setLoading("NONE");
-        if (e.request) {
-          setError(e.message);
-        } else {
-          throw e;
-        }
-      });
-    } else {
-      setLoading("NONE");
-      setFileData(null);
-      setImageData(null);
-    }
-  }, [fileID, personalToken]);
-
   return (
     <Container>
       <Form onSubmit={handleApiInfoChange} />
-      { {"NONE" : null, "LOADING_DOCUMENT": "Loading document...", "LOADING_IMAGES": "Loading images...", "LINTING": "Linting..."}[loading] }
-      {error ? error : null}
-      {fileData ? <Report fileID={fileID} fileData={fileData} summary={summary} imageData={imageData} lintErrors={lintErrors} /> : null}
+      {fileID && personalToken ? (
+        <File fileID={fileID} personalToken={personalToken} />
+      ) : null}
     </Container>
   );
 }
---
+++ src/File.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import Report from "./Report";
+import {fetchDocument, FileData, ImageData, fetchImages} from "./api";
+import { componentSummary, ComponentSummary } from './analysis';
+
+type FileProps = {
+  fileID: string,
+  personalToken: string
+};
+
+export default function File({ fileID, personalToken }:FileProps) {
+  const [fileData, setFileData] = useState<FileData>(null);
+  const [summary, setSummary] = useState<ComponentSummary | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<
+    "LOADING_DOCUMENT" | "LOADING_IMAGES" | "LINTING" | "NONE"
+  >("NONE");
+  const [imageData, setImageData] = useState<ImageData>(null);
+  const [lintErrors, setLintErrors] = useState<any>(null);
+
+  useEffect(() => {
+    if (fileID && personalToken) {
+      const loadEverything = async () => {
+        setLoading("LOADING_DOCUMENT");
+        setFileData(null);
+        setImageData(null);
+        setError(null);
+        const fileData = await fetchDocument(fileID, personalToken);
+        setFileData(fileData);
+
+        if (fileData) {
+          const summary = componentSummary(fileData);
+          setSummary(summary);
+        }
+
+        setLoading("LOADING_IMAGES");
+        const componentIds = fileData ? Object.keys(fileData.components) : [];
+        const images = await fetchImages(fileID, personalToken, componentIds);
+        setImageData(images);
+
+        setLoading("NONE");
+      };
+      loadEverything().catch(e => {
+        setLoading("NONE");
+        if (e.request) {
+          setError(e.message);
+        } else {
+          throw e;
+        }
+      });
+    } else {
+      setLoading("NONE");
+      setFileData(null);
+      setImageData(null);
+    }
+  }, [fileID, personalToken]);
+
+  return (
+    <div>
+      {
+        {
+          NONE: null,
+          LOADING_DOCUMENT: "Loading document...",
+          LOADING_IMAGES: "Loading images...",
+          LINTING: "Linting..."
+        }[loading]
+      }
+      {error ? error : null}
+      {fileData ? (
+        <Report
+          fileID={fileID}
+          fileData={fileData}
+          summary={summary}
+          imageData={imageData}
+          lintErrors={lintErrors}
+        />
+      ) : null}
+    </div>
+  );
+}
