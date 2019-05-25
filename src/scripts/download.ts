8206fe33f5d330b942330a7fa31a363cfb5c7460
Jamie Mill <jamiermill@gmail.com>
2019-05-25 15:03:13 +0100

    Also fetch style metadata in download script

--- src/scripts/download.ts
+++ src/scripts/download.ts
@@ -1,12 +1,23 @@
-import { fetchDocument } from "../api";
+import { fetchDocument, fetchStyle } from "../api";
 
 const personalToken = process.argv[2];
+if (!personalToken) throw "No token";
 const fileID = process.argv[3];
-
-console.log(`fileID: ${fileID}`);
-console.log(`personalToken: ${personalToken}`);
+if (!fileID) throw "No file ID";
 
 (async () => {
-  const document = await fetchDocument(fileID, personalToken);
-  console.log(JSON.stringify(document, null, "  "));
+  console.error(`Fetching document ${fileID}...`);
+  const documentResponse = await fetchDocument(fileID, personalToken);
+
+  let styleResponses = [];
+  if (documentResponse) {
+    for (let styleID in documentResponse.styles) {
+      const key = documentResponse.styles[styleID].key;
+      console.error(`Fetching style ${key}...`);
+      const style = await fetchStyle(key, personalToken);
+      styleResponses.push(style);
+    }
+  }
+  const data = { documentResponse, styleResponses };
+  console.log(JSON.stringify(data, null, "  "));
 })().catch(e => console.error(e.message));
