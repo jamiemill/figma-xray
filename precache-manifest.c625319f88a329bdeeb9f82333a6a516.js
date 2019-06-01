58e68a6668302da09802ebfa1449a26dec583a50
Jamie Mill <jamiermill@gmail.com>
2019-06-01 12:00:37 +0100

    Updates

---
+++ precache-manifest.c625319f88a329bdeeb9f82333a6a516.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51ea6d736e90b0cf946b2afe8c89682",
+    "url": "/figma-xray/0478b12a33042093fa1a.worker.js"
+  },
+  {
+    "revision": "85fd2d4016f6ac6e043a055bc5e7be5e",
+    "url": "/figma-xray/65a162d46459c0c96bd6.worker.js"
+  },
+  {
+    "revision": "e85c8b9548b81e64c849b12d74ef00d7",
+    "url": "/figma-xray/index.html"
+  },
+  {
+    "revision": "00fa1b2741134f98d54c",
+    "url": "/figma-xray/static/js/2.503e4799.chunk.js"
+  },
+  {
+    "revision": "04233a1da7760cd82638",
+    "url": "/figma-xray/static/js/main.bc338d36.chunk.js"
+  },
+  {
+    "revision": "e8230b4cd902dd8789d1",
+    "url": "/figma-xray/static/js/runtime~main.df9937ea.js"
+  },
+  {
+    "revision": "483685cf173f07a971b16c94360e67fe",
+    "url": "/figma-xray/static/media/ComponentIcon.483685cf.svg"
+  },
+  {
+    "revision": "6e637937e2605440bd0cf98484ee80d5",
+    "url": "/figma-xray/static/media/PageIcon.6e637937.svg"
+  },
+  {
+    "revision": "b2843f218909d61098617419b354935f",
+    "url": "/figma-xray/static/media/TextIcon.b2843f21.svg"
+  }
+]);
