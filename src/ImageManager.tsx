f39f1c35ca955fe8d6686baea73d7c73954400c0
Jamie Mill <jamiermill@gmail.com>
2019-06-02 17:51:19 +0100

    Make ImageManager queue and batch image requests and fetch a batch at maximum once every 1 second.

--- src/ImageManager.tsx
+++ src/ImageManager.tsx
@@ -1,9 +1,19 @@
 import { fetchImages } from "./api";
 
+type QueuedImageRequest = {
+  nodeID: string;
+  resolve: (url: string) => void;
+  reject: (reason: any) => void;
+};
+
+const API_REQUEST_INTERVAL = 1000;
+
 export default class ImageManager {
   personalToken: string;
   fileID: string;
   cache: { [nodeID: string]: string };
+  queue: Array<QueuedImageRequest>;
+  intervalID: number;
 
   constructor({
     personalToken,
@@ -15,15 +25,52 @@ export default class ImageManager {
     this.personalToken = personalToken;
     this.fileID = fileID;
     this.cache = {};
+    this.queue = [];
+    this.intervalID = setInterval(() => {
+      this._processQueue();
+    }, API_REQUEST_INTERVAL);
+  }
+
+  destroy() {
+    clearInterval(this.intervalID);
   }
 
   async getPreview(nodeID: string) {
     if (this.cache[nodeID]) {
       return this.cache[nodeID];
     }
-    const images = await fetchImages(this.fileID, this.personalToken, [nodeID]);
-    const url = images[nodeID];
+    const url = await this._enqueue(nodeID);
     this.cache[nodeID] = url;
     return url;
+  }
+
+  async _enqueue(nodeID: string) {
+    const promise = new Promise<string>((resolve, reject) => {
+      this.queue.push({ nodeID, resolve, reject });
+    });
+    return promise;
+  }
+
+  async _fetchImages(nodeIDs: string[]) {
+    return await fetchImages(this.fileID, this.personalToken, nodeIDs);
+  }
+
+  async _processQueue() {
+    if (this.queue.length === 0) {
+      return;
+    }
+    const queueContents = this.queue;
+    console.time("REQUESTING_IMAGES");
+    this.queue = [];
+    const images = await this._fetchImages(queueContents.map(_ => _.nodeID));
+    console.timeEnd("REQUESTING_IMAGES");
+    Object.keys(images).forEach(nodeID => {
+      const url = images[nodeID];
+      const queuedImageRequest =
+        queueContents && queueContents.find(_ => _.nodeID === nodeID);
+      if (queuedImageRequest) {
+        queuedImageRequest.resolve(url);
+      }
+    });
   }
 }
